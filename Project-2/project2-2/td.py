90783c223f3f59260d1dfc198ecc652d9e01f88b
bucky1995 <xinbo1995@outlook.com>
2019-09-25 22:50:53 -0400

    p2-2

--- Project-2/project2-2/td.py
+++ Project-2/project2-2/td.py
@@ -3,7 +3,8 @@
 import numpy as np
 import random
 from collections import defaultdict
-#-------------------------------------------------------------------------
+
+# -------------------------------------------------------------------------
 '''
     Temporal Difference
     In this problem, you will implememnt an AI player for cliffwalking.
@@ -14,9 +15,11 @@ from collections import defaultdict
     You don't have to follow the comments to write your code. They are provided
     as hints in case you need. 
 '''
-#-------------------------------------------------------------------------
 
-def epsilon_greedy(Q, state, nA, epsilon = 0.1):
+
+# -------------------------------------------------------------------------
+
+def epsilon_greedy(Q, state, nA, epsilon=0.1):
     """Selects epsilon-greedy action for supplied state.
     
     Parameters:
@@ -40,13 +43,13 @@ def epsilon_greedy(Q, state, nA, epsilon = 0.1):
     """
     ############################
     # YOUR IMPLEMENTATION HERE #
-
-
-
-
-
+    A = np.ones(nA, dtype=float) * epsilon / nA
+    best_action = np.argmax(Q[state])
+    A[best_action] += (1.0 - epsilon)
+    action = np.random.choice(nA, 1, p=A)
     ############################
     return action
+
 
 def sarsa(env, n_episodes, gamma=1.0, alpha=0.5, epsilon=0.1):
     """On-policy TD control. Find an optimal epsilon-greedy policy.
@@ -72,45 +75,44 @@ def sarsa(env, n_episodes, gamma=1.0, alpha=0.5, epsilon=0.1):
     -----
     You could consider decaying epsilon, i.e. epsilon = 0.99*epsilon during each episode.
     """
-    
+
     # a nested dictionary that maps state -> (action -> action-value)
     # e.g. Q[state] = np.darrary(nA)
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
-    
+
     ############################
     # YOUR IMPLEMENTATION HERE #
-    
+    nA = env.action_space.n
     # loop n_episodes
-
+    for ep in range(n_episodes):
+        print(ep)
         # define decaying epsilon
-
-
-        # initialize the environment 
-
-        
+        epsilon = 0.99*epsilon
+        # initialize the environment
+        state = env.reset()
         # get an action from policy
-
+        action = epsilon_greedy(Q, state, nA, epsilon)[0]
         # loop for each step of episode
-
+        done = False
+        while not done:
             # return a new state, reward and done
-
+            next_state, reward, done, _ = env.step(action)
             # get next action
-
-            
+            next_action = epsilon_greedy(Q, next_state, nA, epsilon)[0]
             # TD update
             # td_target
-
+            td_target = reward + gamma * Q[next_state][next_action]
             # td_error
-
+            td_error = td_target - Q[state][action]
             # new Q
-
-            
+            Q[state][action] += alpha * td_error
             # update state
-
+            state = next_state
             # update action
-
+            action = next_action
     ############################
     return Q
+
 
 def q_learning(env, n_episodes, gamma=1.0, alpha=0.5, epsilon=0.1):
     """Off-policy TD control. Find an optimal epsilon-greedy policy.
@@ -136,29 +138,30 @@ def q_learning(env, n_episodes, gamma=1.0, alpha=0.5, epsilon=0.1):
     # a nested dictionary that maps state -> (action -> action-value)
     # e.g. Q[state] = np.darrary(nA)
     Q = defaultdict(lambda: np.zeros(env.action_space.n))
-    
+
     ############################
     # YOUR IMPLEMENTATION HERE #
-    
+    nA = env.action_space.n
     # loop n_episodes
-
-        # initialize the environment 
-
-        
+    for _ in range(n_episodes):
+        # initialize the environment
+        state = env.reset()
         # loop for each step of episode
-
+        done = False
+        while not done:
             # get an action from policy
-            
+            action = epsilon_greedy(Q, state, nA, epsilon)[0]
             # return a new state, reward and done
-            
+            next_state, reward, done, _ = env.step(action)
             # TD update
+            best_next_action = np.argmax(Q[next_state])
             # td_target with best Q
-
+            td_target = reward + gamma * Q[next_state][best_next_action]
             # td_error
-
+            td_error = td_target - Q[state][action]
             # new Q
-            
+            Q[state][action] += alpha * td_error
             # update state
-
+            state = next_state
     ############################
     return Q
